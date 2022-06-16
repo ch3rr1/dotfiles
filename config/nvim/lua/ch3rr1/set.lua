4df6f0c6d7d150bd0826ffefd35a09796538ccc9
Bastian Kirschner <bastian@ch3rr1.me>
2022-06-16 18:47:51 +0200

    Update set config

--- config/nvim/lua/ch3rr1/set.lua
+++ config/nvim/lua/ch3rr1/set.lua
@@ -22,7 +22,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.incsearch = true
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 -- vim.opt.ignorecase = true
 -- vim.opt.smartcast = true
 
