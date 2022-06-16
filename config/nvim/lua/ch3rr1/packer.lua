fe6c70cf35f083f2dee53bb41abd82ca44db648a
Bastian Kirschner <bastian@ch3rr1.me>
2022-06-16 16:37:57 +0200

    Add plenary plugin

--- config/nvim/lua/ch3rr1/packer.lua
+++ config/nvim/lua/ch3rr1/packer.lua
@@ -2,5 +2,7 @@ return require("packer").startup(function()
     -- Packer can manage itself
     use("wbthomason/packer.nvim")
 
+    use("nvim-lua/plenary.nvim")
+
     use("mbbill/undotree")
 end)
