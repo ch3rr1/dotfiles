439c232f1abfea675a417dad97ab595f664bc59a
Bastian Kirschner <bastian@ch3rr1.me>
2022-06-16 18:34:05 +0200

    Add gruvbox colorscheme

--- config/nvim/lua/ch3rr1/init.lua
+++ config/nvim/lua/ch3rr1/init.lua
@@ -2,3 +2,4 @@ require("ch3rr1.set");
 require("ch3rr1.packer");
 require("ch3rr1.keymap");
 require("ch3rr1.telescope");
+require("ch3rr1.color");
--- config/nvim/lua/ch3rr1/packer.lua
+++ config/nvim/lua/ch3rr1/packer.lua
@@ -2,6 +2,8 @@ return require("packer").startup(function()
     -- Packer can manage itself
     use("wbthomason/packer.nvim")
 
+    use("ellisonleao/gruvbox.nvim")
+
     use("nvim-lua/plenary.nvim")
 
     use("nvim-telescope/telescope.nvim")
