8f5c7219cc632256397e481814c0196a38a347e9
Bastian Kirschner <bastian@ch3rr1.me>
2022-06-16 17:06:33 +0200

    Add basic telescope keymaps

--- config/nvim/lua/ch3rr1/keymap.lua
+++ config/nvim/lua/ch3rr1/keymap.lua
@@ -9,4 +9,13 @@ end
 local nnoremap = M.nnoremap
 
 nnoremap("<leader>pv", ":Ex<CR>")
+
 nnoremap("<leader>u", ":UndotreeToggle<CR>")
+
+nnoremap("<C-p>", ":lua require('telescope.builtin').git_files()<CR>")
+nnoremap("<leader>pf", ":lua require('telescope.builtin').find_files()<CR>")
+nnoremap("<leader>pb", ":lua require('telescope.builtin').buffers()<CR>")
+nnoremap("<leader>ph", ":lua require('telescope.builtin').help_tags()<CR>")
+nnoremap("<leader>pg", ":lua require('telescope.builtin').live_grep()<CR>")
+nnoremap("<leader>ps", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input(\"Grep For > \") })<CR>")
+nnoremap("<leader>pw", ":lua require('telescope.builtin').grep_string({ search = vim.fn.expand(\"<cword>\") })<CR>")
