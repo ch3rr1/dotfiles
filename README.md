fe50ec2176e96bd0d4f39fcb0f425698b8324a20
Bastian Kirschner <bastian@ch3rr1.me>
2022-06-17 01:11:15 +0200

    Add setup config for lua language server

--- README.md
+++ README.md
@@ -16,4 +16,5 @@
 
 ESLint: `npm i -g vscode-langservers-extracted`
 Go: `go install golang.org/x/tools/gopls@latest`
+Lua: [lua-language-server](https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua)
 TypeScript: `npm install -g typescript typescript-language-server`
--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -1,7 +1,40 @@
 local nvim_lsp = require("lspconfig")
 
-nvim_lsp.eslint.setup({})
+local nnoremap = require("ch3rr1.keymap").nnoremap
 
-nvim_lsp.gopls.setup({})
+local function config(_config)
+    return vim.tbl_deep_extend("force", {
+        on_attach = function()
+            nnoremap("K", ":lua vim.lsp.buf.hover()<CR>")
+        end
+    }, _config or {})
+end
 
-nvim_lsp.tsserver.setup({})
+nvim_lsp.eslint.setup(config())
+
+nvim_lsp.gopls.setup(config())
+
+local sumneko_root_path = os.getenv("HOME") .. "/Sources/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
+nvim_lsp.sumneko_lua.setup(config({
+    cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+                path = vim.split(package.path, ";")
+            },
+            diagnostics = {
+                globals = { "vim" }
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+                }
+            }
+        }
+    }
+}))
+
+nvim_lsp.tsserver.setup(config())
