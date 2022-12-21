7894371dd2bd5ebfa8159d23a474181a7c626f9f
Bastian Kirschner <bastian@ch3rr1.me>
2022-12-21 17:26:08 +0100

    nvim: Include hidden files in telescope file search

--- config/nvim/after/plugin/keymap/telescope.lua
+++ config/nvim/after/plugin/keymap/telescope.lua
@@ -2,7 +2,7 @@ local Remap = require("ch3rr1.keymap")
 local nnoremap = Remap.nnoremap
 
 nnoremap("<C-p>", ":lua require('telescope.builtin').git_files()<CR>")
-nnoremap("<leader>pf", ":lua require('telescope.builtin').find_files()<CR>")
+nnoremap("<leader>pf", ":lua require('telescope.builtin').find_files({ hidden = true })<CR>")
 nnoremap("<leader>pb", ":lua require('telescope.builtin').buffers()<CR>")
 nnoremap("<leader>ph", ":lua require('telescope.builtin').help_tags()<CR>")
 nnoremap("<leader>pg", ":lua require('telescope.builtin').live_grep()<CR>")
