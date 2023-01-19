9b2639f3681324a3b1474463fde482c3eaae400f
Bastian Kirschner <bastian@ch3rr1.me>
2023-01-19 20:33:16 +0100

    nvim: Update color scheme setup

--- config/nvim/after/plugin/color.lua
+++ config/nvim/after/plugin/color.lua
@@ -1,21 +1,20 @@
 vim.g.ch3rr1_colorscheme = "gruvbox"
 
---[[ function SetupColorScheme()
+local function defaultSetup()
     -- vim.g.gruvbox_contrast_dark = "hard"
     -- vim.g.gruvbox_transparent_bg = 1
     -- vim.g.gruvbox_invert_selection = false
     -- vim.g.gruvbox_sign_column = "bg0"
-    vim.g.tokyonight_transparent = true
-
-    vim.opt.background = "dark"
-
-    vim.cmd("colorscheme " .. vim.g.ch3rr1_colorscheme)
+    -- vim.g.tokyonight_transparent = true
 
     local hl = function(group, opts)
         vim.api.nvim_set_hl(0, group, opts)
     end
 
     hl("Normal", {
+        bg = "none"
+    })
+    hl("NormalFloat", {
         bg = "none"
     })
     hl("EndOfBuffer", {
@@ -34,51 +33,66 @@ vim.g.ch3rr1_colorscheme = "gruvbox"
     hl("StatusLine", {
         bg = "none"
     })
-    hl("ColorColumn", {
-        bg = "#664c45"
-    })
-    hl("LineNr", {
-        fg = "#fabd2f"
-    })
+    -- hl("ColorColumn", {
+    --     bg = "#664c45"
+    -- })
+    -- hl("LineNr", {
+    --     fg = "#fabd2f"
+    -- })
 end
 
-SetupColorScheme() ]]
+local function gruvboxSetup()
+	require("gruvbox").setup({
+		contrast = "hard",
+		overrides = {
+			Normal = {
+				bg = "none"
+			},
+			SignColumn = {
+				bg = "none"
+			},
+			VertSplit = {
+				bg = "none"
+			},
+			DiagnosticSignError = {
+				bg = "none",
+				fg = "#fb4934"
+			},
+			DiagnosticSignWarn = {
+				bg = "none",
+				fg = "#fabd2f"
+			},
+			DiagnosticSignInfo = {
+				bg = "none",
+				fg = "#83a598"
+			},
+			DiagnosticSignHint = {
+				bg = "none",
+				fg = "#8ec07c"
+			}
+		}
+	})
+end
 
-require("gruvbox").setup({
-    contrast = "hard",
-    overrides = {
-        Normal = {
-            bg = "none"
-        },
-        SignColumn = {
-            bg = "none"
-        },
-        VertSplit = {
-            bg = "none"
-        },
-        DiagnosticSignError = {
-            bg = "none",
-            fg = "#fb4934"
-        },
-        DiagnosticSignWarn = {
-            bg = "none",
-            fg = "#fabd2f"
-        },
-        DiagnosticSignInfo = {
-            bg = "none",
-            fg = "#83a598"
-        },
-        DiagnosticSignHint = {
-            bg = "none",
-            fg = "#8ec07c"
-        }
-    }
-})
+local colorSchemes = {
+	["gruvbox"] = gruvboxSetup
+}
 
-require("catppuccin").setup({
-	transparent_background = true
-})
+function SetupColorScheme(color)
+	color = color or "rose-pine"
 
-vim.opt.background = "dark"
+	-- vim.opt.background = "dark"
 
-vim.cmd("colorscheme " .. vim.g.ch3rr1_colorscheme)
+	local setupFunction = colorSchemes[color]
+	if setupFunction then
+		setupFunction()
+	else
+		defaultSetup()
+	end
+
+	vim.cmd("colorscheme " .. vim.g.ch3rr1_colorscheme)
+end
+
+SetupColorScheme(vim.g.ch3rr1_colorscheme)
+
+
