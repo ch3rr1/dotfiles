1f5f6c7e18f9a3cb8cbe5b7d4744b342eed24acc
Bastian Kirschner <bastian@ch3rr1.me>
2023-01-19 21:03:39 +0100

    nvim: Add treesitter auto load

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = "all",
+    ensure_installed = {"help", "javascript", "typescript", "rust", "c", "lua"},
     sync_install = false,
+	auto_install = true,
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
