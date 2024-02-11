3e7c5100cbfb829efe192699960aaa4ee0482ba3
Bastian Kirschner <bastian@ch3rr1.me>
2024-02-11 18:05:28 +0100

    nvim: Prettify

--- config/nvim/lua/ch3rr1/packer.lua
+++ config/nvim/lua/ch3rr1/packer.lua
@@ -45,7 +45,7 @@ return require("packer").startup(function(use)
     -- use("TimUntersberger/neogit", {
     --     requires = "nvim-lua/plenary.nvim"
     -- })
-	
+
 	use({
 		"folke/trouble.nvim",
 		config = function()
