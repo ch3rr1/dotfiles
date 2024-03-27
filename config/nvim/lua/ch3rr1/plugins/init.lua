91df6f37cf6c8c7e6fcaa594c127fc9d841266fb
Bastian Kirschner <bastian@ch3rr1.me>
2024-03-27 22:45:56 +0100

    nvim: Add copilot plugin

--- config/nvim/lua/ch3rr1/plugins/init.lua
+++ config/nvim/lua/ch3rr1/plugins/init.lua
@@ -129,6 +129,8 @@ return {
         },
     },
 
+    { 'github/copilot.vim' },
+
     -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
     -- put them in the right spots if you want.
