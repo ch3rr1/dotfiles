def3bf2e95f8641be1f07f75fb5f14d5d972b3b2
Bastian Kirschner <bastian@ch3rr1.me>
2025-11-10 19:23:22 +0100

    neovim: add bashls to lsp servers

--- config/nvim/lua/ch3rr1/plugins/lsp.lua
+++ config/nvim/lua/ch3rr1/plugins/lsp.lua
@@ -74,7 +74,9 @@ return { -- LSP Configuration & Plugins
         local servers = {
             -- clangd = {},
             -- gopls = {},
+            -- in your servers.pyright table
             pyright = {},
+            bashls = {},
             -- rust_analyzer = {},
             -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
             --
